94740e2c56c0865893976b36503b3aedf2ce4131
rahulnehru <rnehru@deloitte.co.uk>
2019-11-02 21:09:18 +0000

    makes match a class

--- src/main/java/match/Match.java
+++ src/main/java/match/Match.java
@@ -5,23 +5,23 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
-public interface Match<T> {
+public class Match {
 
-    static <T> void match(T o, Consumer... a) {
+    public static <T> void match(T o, Consumer... a) {
         for (Consumer consumer : a)
             consumer.accept(o);
     }
 
-    static <T> Consumer caze(Class<T> cls, Consumer<T> c) {
+    public static <T> Consumer caze(Class<T> cls, Consumer<T> c) {
         return obj -> Optional.of(obj).filter(cls::isInstance).map(cls::cast).ifPresent(c);
     }
 
-    static <T> Consumer caze(Class<T> cls, Predicate<T> p, Consumer<T> c) {
+    public static <T> Consumer caze(Class<T> cls, Predicate<T> p, Consumer<T> c) {
         return obj -> Optional.of(obj).filter(cls::isInstance).map(cls::cast).filter(p).ifPresent(c);
     }
 
 
-    static <T,R> R match(T s, Function... collectors) {
+    public static <T,R> R match(T s, Function... collectors) {
         for (Function collector: collectors) {
             Optional<R> r = (Optional<R>) collector.apply(s);
             if(r.isPresent()) return r.get();
@@ -30,11 +30,11 @@ public interface Match<T> {
     }
 
 
-    static <T, R> Function<T, Optional<R>> map(Class<T> cls, Function<T, R> c) {
+    public static <T, R> Function<T, Optional<R>> map(Class<T> cls, Function<T, R> c) {
         return obj -> Optional.of(obj).filter(cls::isInstance).map(cls::cast).map(c);
     }
 
-    static <T, R> Function<T, Optional<R>> map(Class<T> cls, Predicate<T> p, Function<T, R> c) {
+    public static <T, R> Function<T, Optional<R>> map(Class<T> cls, Predicate<T> p, Function<T, R> c) {
         return obj -> Optional.of(obj).filter(cls::isInstance).map(cls::cast).filter(p).map(c);
     }
 
