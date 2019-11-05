b3a6a129766d396107f6a57390859ebe09f3e27f
rahulnehru <rnehru@deloitte.co.uk>
2019-11-05 09:16:31 +0000

    added javadoc for validated

--- src/main/java/com/rnehru/langutils/validated/Validated.java
+++ src/main/java/com/rnehru/langutils/validated/Validated.java
@@ -5,7 +5,45 @@ import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
+/**
+ * Validated provides a facade for performing concise and reusable validation of objects.
+ * There are two implementations of Validated - the Valid and Invalid classes.
+ *
+ * Consider the following code:
+ * <pre>
+ * {@code
+ *      class User {
+ *          private String name;
+ *          private int age;
+ *          public User(String name, int age) {
+ *              this.name = name;
+ *              this.age = age;
+ *          }
+ *      }
+ *
+ *      Predicate<User> isAdult = u -> u.age > 18;
+ *      Predicate<User> isCalledTom = u -> u.name.equals("Tom");
+ *
+ *      combine(
+ *          validate(isAdult, "validation failed, user is not an adult"),
+ *          validate(isCalledTom, "validation failed, user is called Tom")
+ *      ).apply(new User("Tom", 15));
+ * }
+ * </pre>
+ *
+ *
+ */
 public interface Validated {
+
+    /** validate returns a function which applies a predicate condition to an object, and returns either a Valid or
+     * Invalid when predicates are met.
+     *
+     * @param p predicate function operating on the applied object
+     * @param e validation failed object to encapsulate in the Invalid
+     * @param <T> type of object on which the predicate is being applied
+     * @param <E> type of the validation failed object to be encapsulated in the Invalid
+     * @return returns a function that can be applied with a an object of type T
+     */
 
     static <T, E> Function<T, Validated> validate(Predicate<T> p, E e) {
         return t -> p.test(t) ? Valid.of(t) : Invalid.of(e);
--- src/test/java/com/rnehru/langutils/validated/ValidatedTest.java
+++ src/test/java/com/rnehru/langutils/validated/ValidatedTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import java.util.List;
 import java.util.function.Predicate;
 
+import static com.rnehru.langutils.validated.Validated.combine;
+import static com.rnehru.langutils.validated.Validated.validate;
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertFalse;
@@ -15,7 +17,7 @@ public class ValidatedTest {
     public void validateReturnsValidWhenValidationPasses() throws ValidationProjectionError {
         Predicate<Integer> predicate = i -> i > 3;
 
-        Validated i = Validated.validate(predicate, "Help").apply(4);
+        Validated i = validate(predicate, "Help").apply(4);
 
         assertTrue(i.isValid());
         assertEquals(4, i.projectValid().getInternal());
@@ -25,7 +27,7 @@ public class ValidatedTest {
     public void validateReturnsInvalidWhenValidationFails() throws ValidationProjectionError {
         Predicate<Integer> predicate = i -> i > 3;
 
-        Validated i = Validated.validate(predicate, "Help").apply(2);
+        Validated i = validate(predicate, "Help").apply(2);
 
         assertFalse(i.isValid());
         assertEquals("Help", i.projectInvalid().getInternal());
@@ -35,7 +37,7 @@ public class ValidatedTest {
     public void projectValidThrowsValidationProjectionExceptionWhenInvalid() {
         Predicate<Integer> predicate = i -> i > 3;
 
-        Validated i = Validated.validate(predicate, "Help").apply(2);
+        Validated i = validate(predicate, "Help").apply(2);
 
         i.projectValid().getInternal();
     }
@@ -44,7 +46,7 @@ public class ValidatedTest {
     public void projectInvalidThrowsValidationProjectionExceptionWhenInvalid() {
         Predicate<Integer> predicate = i -> i > 1;
 
-        Validated i = Validated.validate(predicate, "Help").apply(2);
+        Validated i = validate(predicate, "Help").apply(2);
 
         i.projectInvalid().getInternal();
     }
@@ -55,27 +57,23 @@ public class ValidatedTest {
         Predicate<User> userIsNotPensionable = u -> u.age < 65;
         Predicate<User> userIsJohn = u -> u.name.contains("John");
 
-        User u1 = new User("John", 34);
-        User u2 = new User("Tim", 67);
+        Validated validatedJohn = combine(
+                validate(userIsJohn, "User is not John"),
+                validate(userIsWorkingAdult, "User is not over 18"),
+                validate(userIsNotPensionable, "User is not under 65")
+        ).apply(new User("John", 34));
+
+        Validated validatedTim = combine(
+                validate(userIsJohn, "User is not John"),
+                validate(userIsWorkingAdult, "User is not over 18"),
+                validate(userIsNotPensionable, "User is not under 65")
+        ).apply(new User("Tim", 67));
 
 
-        Validated john = Validated.combine(
-                Validated.validate(userIsJohn, "User is not John"),
-                Validated.validate(userIsWorkingAdult, "User is not over 18"),
-                Validated.validate(userIsNotPensionable, "User is not under 65")
-        ).apply(u1);
+        assertTrue(validatedJohn.isValid());
+        assertFalse(validatedTim.isValid());
 
-        Validated tim = Validated.combine(
-                Validated.validate(userIsJohn, "User is not John"),
-                Validated.validate(userIsWorkingAdult, "User is not over 18"),
-                Validated.validate(userIsNotPensionable, "User is not under 65")
-        ).apply(u2);
-
-
-        assertTrue(john.isValid());
-        assertFalse(tim.isValid());
-
-        List<Invalid> reasons = (List<Invalid>) tim.projectInvalid().getInternal();
+        List<Invalid> reasons = (List<Invalid>) validatedTim.projectInvalid().getInternal();
         assertEquals(2, reasons.size());
         assertEquals("User is not John", reasons.get(0).getInternal());
         assertEquals("User is not under 65", reasons.get(1).getInternal());
